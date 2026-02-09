6303c2a1e1effd2f0d1726f5616b15d0e6693778
brandontrama <b18tram@siena.edu>
2026-02-09 18:05:43 -0500

    Integrated ability to deploy app using Firebase Hosting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "src",
+  //base: '/vite-firebase-auth/',
   server: {
     host: "127.0.0.1",
     port: 5173
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        home: resolve(__dirname, "src/home.html")
+      }
+    }
   }
 });
